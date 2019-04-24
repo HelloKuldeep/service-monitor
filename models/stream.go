8e5c468efe013b950f10600ed72f12b0b04816e3
hellokuldeep <kul123sharma@gmail.com>
2019-04-24 14:34:28 +0530

    v1.4-Minor-Change

--- models/stream.go
+++ models/stream.go
@@ -2,8 +2,7 @@ package models
 
 import "gopkg.in/mgo.v2/bson"
 
-// Represents a movie, we uses bson keyword to tell the mgo driver how to name
-// the properties in mongodb document
+// Represents a stream
 type Stream struct {
 	ID			bson.ObjectId `bson:"_id" json:"id"`
 	HitTime		int        `bson:"hittime" json:"hittime"`
